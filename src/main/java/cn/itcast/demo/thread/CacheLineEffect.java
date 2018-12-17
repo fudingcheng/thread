ed119138568e15cf9c51bafa8044309ce5108b93
fudingcheng <342240165@qq.com>
2018-12-17 16:48:23 +0800

    使用缓存行提高性能 大数组嵌套小数组遍历 和 小数组嵌套大数组遍历的性能比较

---
+++ src/main/java/cn/itcast/demo/thread/CacheLineEffect.java
@@ -0,0 +1,39 @@
+package cn.itcast.demo.thread;
+
+/**
+ * 使用缓存行提高性能
+ *
+ *  显示结果:
+ *      大数组嵌套小数组:Loop times:16ms
+ *      小数组嵌套大数组:Loop times:62ms
+ */
+public class CacheLineEffect {
+    //考虑一般缓存行大小是64字节，一个 long 类型占8字节
+    static  long[][] arr;
+
+    public static void main(String[] args) {
+        arr = new long[1024 * 1024][];
+        for (int i = 0; i < 1024 * 1024; i++) {
+            arr[i] = new long[8];
+            for (int j = 0; j < 8; j++) {
+                arr[i][j] = 0L;
+            }
+        }
+        long sum = 0L;
+        long marked = System.currentTimeMillis();
+        for (int i = 0; i < 1024 * 1024; i+=1) {
+            for(int j =0; j< 8;j++){
+                sum = arr[i][j];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+
+        marked = System.currentTimeMillis();
+        for (int i = 0; i < 8; i+=1) {
+            for(int j =0; j< 1024 * 1024;j++){
+                sum = arr[j][i];
+            }
+        }
+        System.out.println("Loop times:" + (System.currentTimeMillis() - marked) + "ms");
+    }
+}
